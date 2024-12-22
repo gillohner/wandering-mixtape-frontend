bc866d67d3e15ac138fdc78176835379a2b15387
Gil Lohner <gil@lohner.net>
2024-12-22 15:07:37 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+import React from 'react';
+import './App.css';
+import Map from './Map.tsx';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>✈️ WanderingMixedTape 💿</h1>
+            <Map />
+        </div>
+    );
+};
+
+export default App;
